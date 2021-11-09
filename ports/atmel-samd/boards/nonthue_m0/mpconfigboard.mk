41d1d1ad87b2504aeedb49c70faa5c536b12c3bf
ELTECH <55095931+eltech-argentina@users.noreply.github.com>
2021-11-09 18:28:09 -0300

    Update mpconfigboard.mk

--- ports/atmel-samd/boards/nonthue_m0/mpconfigboard.mk
+++ ports/atmel-samd/boards/nonthue_m0/mpconfigboard.mk
@@ -1,5 +1,5 @@
 USB_VID = 0x239A
-USB_PID = 0xED36 #0x8023
+USB_PID = 0xED36
 USB_PRODUCT = "Eltech Nonthue M0"
 USB_MANUFACTURER = "Adafruit Industries LLC"
 
@@ -10,4 +10,5 @@ SPI_FLASH_FILESYSTEM = 1
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ, GD25Q16C"
 LONGINT_IMPL = MPZ
 
+# Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
